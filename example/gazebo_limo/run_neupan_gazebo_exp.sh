7066d0374b9a7e1805d31b490029a4882528c13d
han <744475118@qq.com>
2025-04-08 02:35:21 +0800

    add 10 obstacles world

--- example/gazebo_limo/run_neupan_gazebo_exp.sh
+++ example/gazebo_limo/run_neupan_gazebo_exp.sh
@@ -6,5 +6,5 @@ gnome-terminal -- bash -c "roslaunch neupan_ros gazebo_limo_env_complex_20.launc
 # Give some time for Gazebo to start
 sleep 15
 
-# Step 2: Launch the RDA control with obstacle information from laser scan
+# Step 2: Launch the NeuPAN control with obstacle information from laser scan
 gnome-terminal -- bash -c "roslaunch neupan_ros neupan_gazebo_limo.launch"
